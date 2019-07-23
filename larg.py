a39c5a5f29b0190452348a5cb62df6d0db493930
kevinrajk <51223105+kevinrajk@users.noreply.github.com>
2019-07-23 13:32:31 +0530

    Update larg.py

--- larg.py
+++ larg.py
@@ -1,12 +1,3 @@
-na1,na2,na3 = map(int,input().split())
-
- 
-if (na1 > na2) and (na1 > na3):
-   largest = na1
-elif (na2 > na1) and (na2 > na3):
-   largest = na2
-else:
-   largest = na3
- 
-print("",largest)
+l=list(map(int,input().split()))
+print(max(l))
 
