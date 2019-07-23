98c5013952025590a45f9b9fde9ca14e7d79cb11
kevinrajk <51223105+kevinrajk@users.noreply.github.com>
2019-07-23 17:02:33 +0530

    Update pr3.py

--- pr3.py
+++ pr3.py
@@ -1,13 +1,8 @@
-x,y=map(str,input('').split())
-t=[]
-t.append(x)
-t.append(y)
-s=[]
-for i in zip(*l):
-    if i.count(i[0])==len(i):
-        s.append(i[0])
-    else:
-        break
-m=max(t,key=len)
-k=len(m)-len(s)
-print(k)
+i,j=input().split()
+char=abs(len(j)-len(i))
+for g in range(len(i)):
+  if(len(j)==1 and b[g] in i):
+    break
+  if(i[g]!=j[g]):
+    char=char+1
+print(char)
