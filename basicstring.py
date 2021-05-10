07658320644f04684580ad524c70b7b6df56e0a3
kevinrajk <51223105+kevinrajk@users.noreply.github.com>
2021-05-10 13:11:47 +0530

    Create basicstring.py

---
+++ basicstring.py
@@ -0,0 +1,12 @@
+print("Hello World")
+print("Hello World")
+print("Hello ")
+a=10
+b=20
+c=a+b
+print(c)
+mystring='hi python'+' programming'
+print(mystring[2:9])
+print(mystring.split(' '))
+print(mystring.find('p'))
+print(mystring.replace('hi','hello'))
