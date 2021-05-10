051235ef5229ea8673e91b8a8926e1091475d260
kevinrajk <51223105+kevinrajk@users.noreply.github.com>
2021-05-10 13:28:02 +0530

    Update basicstring.py

--- basicstring.py
+++ basicstring.py
@@ -1,4 +1,5 @@
-print("Hello World")
+program:
+  
 print("Hello World")
 print("Hello ")
 a=10
@@ -10,3 +11,17 @@ print(mystring[2:9])
 print(mystring.split(' '))
 print(mystring.find('p'))
 print(mystring.replace('hi','hello'))
+print("the length of the string is",len(mystring))
+
+
+
+output:
+  
+Hello World                                                                                                                   
+Hello                                                                                                                         
+30                                                                                                                            
+ python                                                                                                                       
+['hi', 'python', 'programming']                                                                                               
+3      
+hello python programming                                                                                                      
+the length of the string is 21
